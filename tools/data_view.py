f120721b87c180c049513b8bd7ba73ebcea49cd2
DaiGuard <zenith.or.w@gmail.com>
2023-11-03 23:27:53 +0900

    学習データ修正機能の追加

--- tools/data_view.py
+++ tools/data_view.py
@@ -34,10 +34,32 @@ if __name__ == '__main__':
             
             dataset.append(d)
 
-    cv2.namedWindow('data_view', cv2.WINDOW_NORMAL)
-    cv2.createTrackbar('ID', 'data_view', 0, len(dataset)-1, lambda val: print(val))
-
     id = -1
+    width = -1
+    height = -1
+
+    def mouseEvents(event, x, y, flags, param):
+        global id, dataset, width, height
+
+        try:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                dataset[id][1] = f'{1.0 - y * 2.0 / height}'
+                dataset[id][2] = f'{1.0 - x * 2.0 / width}'
+            # elif event == cv2.EVENT_LBUTTONDOWN:
+            #     if id < len(dataset) - 1:
+            #         cv2.setTrackbarPos('ID', 'data_view', id+1)            
+            # elif event == cv2.EVENT_RBUTTONDOWN:
+            #     if id >= 0:
+            #         cv2.setTrackbarPos('ID', 'data_view', id-1)
+
+                # print(dataset[id][1], dataset[id][2])
+        except Exception as e:
+            print(e)
+
+
+    cv2.namedWindow('data_view', cv2.WINDOW_NORMAL)
+    cv2.createTrackbar('ID', 'data_view', 0, len(dataset)-1, lambda val: {})
+    cv2.setMouseCallback('data_view', mouseEvents)    
 
     try:
         while True:
@@ -46,6 +68,9 @@ if __name__ == '__main__':
                 id = current_id
                 image = cv2.imread(os.path.join(args[1], dataset[id][0]))
             
+            width = image.shape[1]
+            height = image.shape[0]
+
             preview = image.copy()
             preview = cv2.rectangle(preview, (0, 0), (640, 130), (0, 0, 0), thickness=-1)
             preview = cv2.rectangle(preview, (150, 260), (490, 360), (0, 0, 0), thickness=-1)
@@ -60,23 +85,22 @@ if __name__ == '__main__':
                 
             cv2.putText(preview, '{:.2f}, {:.2f}'.format(float(dataset[id][1]), float(dataset[id][2])),
                             (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (0, 0, 0), 2, cv2.LINE_4)
+                            (255, 255, 255), 2, cv2.LINE_4)
 
             v_val = float(dataset[id][1])
             u_val = float(dataset[id][2])
 
-            v = int((1.0 - v_val) * image.shape[0] / 2.0)
-            u = int((1.0 - u_val) * image.shape[1] / 2.0)
+            v = int((1.0 - v_val) * height / 2.0)
+            u = int((1.0 - u_val) * width / 2.0)
 
             preview = cv2.circle(preview, (u, v), 10, (255, 0, 0), thickness=-1)
 
             cv2.imshow('data_view', preview)
-            key = cv2.waitKey(30)        
-            print(key, id)
-            if key == 83:
+            key = cv2.waitKey(30)
+            if key == 102:
                 if id < len(dataset) - 1:
                     cv2.setTrackbarPos('ID', 'data_view', id+1)            
-            elif key == 81:
+            elif key == 118:
                 if id >= 0:
                     cv2.setTrackbarPos('ID', 'data_view', id-1)            
             elif key == 101:
